33a7cdebc7400426b099d393dc7c40e375ec5972
Emily Mead <emilymead@u.boisestate.edu>
2022-03-01 21:11:26 -0700

    Finished project

--- src/UnaryMinus.java
+++ src/UnaryMinus.java
@@ -1,11 +1,26 @@
+/**
+ * This class is what deals with muiltiple minus signs.
+ * When there is more then one it will make sure to keep
+ * track if the number is positive or negitive
+ */
 public class UnaryMinus extends NodeFact{
     private NodeFact node;
 
+    /**
+     * Creates a new NodeFact that will be used to keep track of the
+     * current sign standing the of number
+     * @param node
+     */
     public UnaryMinus(NodeFact node){
         this.node = node;
     }
 
-    @Override
+    /**
+     * Switches the sign of the node accordinly
+     * @param env the enviorment that is being used
+     * @return the node with the current negitive or positive flag
+     * @throws EvalException
+     */
     public double eval(Environment env) throws EvalException {
         return -node.eval(env);
     }
